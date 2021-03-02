c9aed3ede23b4bd30ffe2fd9de03cd4c8b0980b6
j-Riv <jriv@suavecito.com>
2021-03-02 09:26:25 -0800

    Moved to Gatsby Cloud - Added Gatsby Cloud Plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,6 +45,7 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-typescript`,
     `gatsby-plugin-styled-components`,
+    `gatsby-plugin-gatsby-cloud`,
     {
       resolve: `gatsby-source-wordpress`,
       options: {
