278ff054911840998ec281e1b9436e19da0586d9
José Alfredo Rivera Turcios <jose.alfredo.rivera@gmail.com>
2021-06-29 22:07:02 -0700

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -100,7 +100,6 @@ export const pageQuery = graphql`
                     placeholder: BLURRED
                     layout: CONSTRAINED
                     aspectRatio: 1
-                    formats: [AUTO, WEBP]
                   )
                 }
               }
